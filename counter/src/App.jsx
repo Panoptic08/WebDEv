d651112dd1a5c26b94765196f2fcd8940cba74f8
Panoptic08 <your_email@example.com>
2024-07-05 19:21:42 +0530

    add all

--- counter/src/App.jsx
+++ counter/src/App.jsx
@@ -2,27 +2,31 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-let count = 0;
-function add(){
-  count+=1;
-  console.log("Value: ",count);
-}
-function sub(){
-  count-=1;
-  console.log("Value2: ",count);
-}
-function App() {
 
+function App() {
+  let [counter,updateme] = useState(0)
+
+  function add(){
+    if(counter<20){updateme(counter+1)}
+    else{
+      alert("Exceeded 20");
+    }
+  }
+
+  function sub(){
+    if(counter>0){updateme(counter-1)}
+    else{
+      alert("Going Negative.");
+    }
+  }
+  
   return (
     <>
-     <h1>Universe</h1>
-
-     <h2>Counter: {count}</h2>
-     <button onClick={add}>+</button>
-     <br /><br />
-     <button onClick={sub}>-</button>
-
-    </>
+    <h1>Universe</h1>
+    <h2>Count on me: {counter}</h2>
+    <button onClick={add}>+</button>
+    <button onClick={sub}>-</button>
+    </>  
   )
 }
 
