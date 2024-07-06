a31142e99319d4de097b12d5de0a1fd099923843
Panoptic08 <your_email@example.com>
2024-07-06 12:51:56 +0530

    1s

--- colors/src/App.jsx
+++ colors/src/App.jsx
@@ -8,25 +8,12 @@ function App() {
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+     <div className='bg-green-400 p-5  justify-content: space-evenly'>
+     <button>1. green</button>
+     <button>2. red</button>
+     <button>3. blue</button>
+     <button>4. grey</button>
+</div>  
     </>
   )
 }
