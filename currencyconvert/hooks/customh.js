63bad786a6baa03a0fbac0df2593bd521e504e29
Panoptic08 <your_email@example.com>
2024-07-09 19:25:47 +0530

    add aall

---
+++ currencyconvert/hooks/customh.js
@@ -0,0 +1,11 @@
+import { useState,useEffect } from "react";
+
+function info(currency){
+    const [data, setdata] = useState({})
+    useEffect(()=>{
+        fetch(`https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies/${currency}.json`).then((res)=>{res.json()}).then((res)=>setdata[res(currency)]) // hold the data.
+    }, [currency])
+    return data;
+}
+
+export default info;
